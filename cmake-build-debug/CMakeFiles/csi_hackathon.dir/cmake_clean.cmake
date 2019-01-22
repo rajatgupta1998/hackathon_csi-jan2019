c4578c7ad5c48e1798c712facf1646934987c482
Rajat Gupta <rjtgupta84@gmail.com>
2019-01-22 17:57:58 +0530

    Add support for CLion
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

---
+++ cmake-build-debug/CMakeFiles/csi_hackathon.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/csi_hackathon.dir/database.cpp.o"
+  "csi_hackathon.pdb"
+  "csi_hackathon"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/csi_hackathon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
